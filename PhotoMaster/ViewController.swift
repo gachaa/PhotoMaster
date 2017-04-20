6e2321e0eb0cfcacb6b62783070a1f56fa6c7fff
gachaa <mae.gachahero.104@gmail.com>
2017-04-20 22:10:11 +0900

    ViewController 54

--- PhotoMaster/ViewController.swift
+++ PhotoMaster/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var photoImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +21,88 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func onTappedCameraButton(){
+        presentPickerController(sourceType: .camera)
+    }
+    
+    @IBAction func onTappedAlbumButton(){
+        presentPickerController(sourceType: .photoLibrary)
+    }
+    
+    @IBAction func onTappedTextButton(){
+        if photoImageView.image != nil {
+            photoImageView.image = drawText(image: photoImageView.image!)
+        } else {
+            print("画像がありません")
+        }
+    }
+    
+    @IBAction func onTappedIllustButton() {
+        if photoImageView.image != nil {
+            photoImageView.image = drawMaskImage(image: photoImageView.image!)
+        } else {
+            print("画像がありません")
+        }
+    }
+    
+    func presentPickerController(sourceType: UIImagePickerControllerSourceType){
+        if UIImagePickerController.isSourceTypeAvailable(sourceType){
+            let picker = UIImagePickerController()
+            
+            picker.sourceType = sourceType
+            picker.delegate = self
+            self.present(picker, animated: true, completion: nil)
+        }
+    }
 
-
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: Any]){
+        self.dismiss(animated: true, completion: nil)
+        
+        photoImageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+    }
+    
+    func drawText(image: UIImage) -> UIImage {
+        let text = "LifeisTech\niPhoneアプリ開発コース！"
+        
+        let textFontAttributes = [
+            NSFontAttributeName: UIFont(name: "Arial", size: 120)!,
+            NSForegroundColorAttributeName: UIColor.red]
+        
+        UIGraphicsBeginImageContext(image.size)
+        image.draw(in: CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height))
+        
+        let margin: CGFloat = 5.0
+        let textRect = CGRect(x: margin, y: margin, width: image.size.width - margin, height: image.size.height - margin)
+        
+        text.draw(in: textRect, withAttributes: textFontAttributes)
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage!
+    }
+    
+    func drawMaskImage(image: UIImage) -> UIImage{
+        let maskImage = UIImage(named: "fin.png")!
+        
+        UIGraphicsBeginImageContext(image.size)
+        
+        image.draw(in: CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height))
+        
+        let margin: CGFloat = 50.0
+        let maskRect = CGRect(x: image.size.width - maskImage.size.width - margin,
+                              y: image.size.height - maskImage.size.height - margin,
+                              width: maskImage.size.width, height: maskImage.size.height)
+        
+        maskImage.draw(in: maskRect)
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        UIGraphicsEndImageContext()
+        
+        return newImage!
+        
+    }
 }
 
