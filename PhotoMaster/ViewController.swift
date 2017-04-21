a0558cc9d86c90010cfd103c1839f8054fc81823
gachaa <mae.gachahero.104@gmail.com>
2017-04-21 15:22:01 +0900

    a

--- PhotoMaster/ViewController.swift
+++ PhotoMaster/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
     @IBOutlet var photoImageView: UIImageView!
 
